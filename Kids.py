5df0c6b4042a1f787e20af81177a5141396005c0
ForeignerfromJupiter <ashik.naruto09@gmail.com>
2021-06-02 02:58:42 +0530

    Kids - Madlibs

---
+++ Kids.py
@@ -0,0 +1,21 @@
+creature = input("Name a Creature: ")
+adj = input("Adjective: ")
+verb1 = input("Verd ending in ed: ")
+feeling = input("Name a Feeling: ")
+clothes = input("Favorite clothes: ")
+sparkly = input("Something Sparkly: ")
+superpower = input("Superpower you wish to have: ")
+food = input("Favorite food: ")
+game = input("Favorite game: ")
+animal = input("Name an Animal: ")
+song = input("Favorite song: ")
+room = input("Name a room in the house: ")
+fav_place = input("Favorite Place: ")
+secret_password = input("Tell us the Secret Password: ")
+madlib = f"Once there was a strange time where a {creature} took over the world \
+and made us all stay {adj} at home. So we {verb1} into our imagination and found \
+out the key to {feeling} was all about wearing {clothes} with {sparkly}. It \
+turned out that this was also the trick to turning on our {superpower} and getting to do everything you love at once. \
+Now, we can eat {food} while playing {game}, or search for {animal} while singing {song}. One time, \
+we even turned {room} into a {fav_place}. Don't belive us? Just say {secret_password} and we'll show you."
+print(madlib)
